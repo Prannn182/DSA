6c3d20e0b944873b59cca0158b4aa9264bf6b4bf
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-03-18 19:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (45.26%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -6,10 +6,15 @@ class Solution {
         int mid=start+(end-start)/2;
         if(target<nums[mid]){
             end=mid-1;
-        }else if(target>nums[mid]){
+        }
+        else if(target>nums[mid]){
             start=mid+1;
-        }else{
+        }
+        else{
             return mid;
         }
-       }return -1;
-    }}
+        }
+          return -1;
+       }}
+          
+     
