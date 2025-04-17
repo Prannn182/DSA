e1bf2d600b5258e9b2d55bf4799e39357bd20bfc
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-04-17 13:56:19 +0530

    Time: 23 ms (99.80%) | Memory: 53.2 MB (67.70%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    let a="Hello World"
+    
+    return function(...args) {
+        return a
+    }
+};
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
