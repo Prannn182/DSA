53d12880cc3c9149b6f9bf865fadb98df9c1eb16
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-04-17 18:00:01 +0530

    Time: 46 ms (34.37%) | Memory: 53.4 MB (61.68%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {return{
+   toBe: function(n){
+    if(val===n)
+    return true 
+    throw new Error("Not Equal")
+   }, notToBe: function(n){
+    if(val!==n)
+    return true
+    throw new Error("Equal")
+   }}
+      };
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
