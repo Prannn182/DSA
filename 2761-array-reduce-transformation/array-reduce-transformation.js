6b443a77a4627215b66f8b2c7bd909ff1f7cbe6e
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-04-20 20:50:40 +0530

    Time: 48 ms (31.59%) | Memory: 53.7 MB (88.09%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,8 +5,8 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let val=init;
-    for(let i=0;i<nums.length;i++){
+    var val=init;
+    for(var i=0;i<nums.length;i++){
         val=fn(val,nums[i])
     }
     return val
