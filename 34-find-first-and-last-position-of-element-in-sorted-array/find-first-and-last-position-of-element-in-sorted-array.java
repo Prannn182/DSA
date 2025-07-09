74001e173aee2b8691507e055db388de7bd3ac7a
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-07-09 19:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (93.60%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,33 +1,31 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = { -1, -1 };
-        int start = search(nums, target, true);
-        int end = search(nums, target, false);
-        ans[0] = start;
-        ans[1] = end;
-        return ans;
-    }
+       int[] ans={-1,-1};
+       int start=search(nums,target,true);
+       int end=search(nums,target,false);
+       ans[0]=start;
+       ans[1]=end;
+       return ans;
 
-int search(int[] nums,int target,boolean firstoccurence){
-    int ans=-1;
-     int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>target){
-                end=mid-1;
-            }
-            else if(target>nums[mid]){
-                 start=mid+1;
-                }
-                else{
-                ans=mid;
-                if(firstoccurence){
-                    end=mid-1;
-                }else{
-                    start=mid+1;
-                }
-            }
-        } return ans;
     }
+ int search(int[] nums,int target, boolean firstoccurence){
+    int ans=-1;
+    int start=0;
+    int end=nums.length-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(target>nums[mid]){
+            start=mid+1;
+        }else if(target<nums[mid]){
+            end=mid-1;
+        }else{
+            ans=mid;
+            if(firstoccurence){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        } 
+    }return ans;
+ }
 }
