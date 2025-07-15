b7c8d92945fe71e45fe4d8c1eb9a3263501d4fda
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-07-15 20:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (55.58%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,13 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-       int start=0;
-       int end=nums.length-1;
-       while(start<end){
-        int mid=start+(end-start)/2;
-        if(nums[mid]<nums[mid+1]){
-            start=mid+1;
-        }else{
-            end=mid;
-        }
-       }return start;
-}}
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[mid+1]){
+                //we are at the decreasing part of array
+                end=mid;
+            }else{
+                // we are the the increasing part of array
+                start=mid+1;
+            } // the loop will will break if and only if when only one element iss remaining or you can say that start==end then the loop will break and we will get our answer
+        } return start; // start==end so we can write anything 
+    }
+}
