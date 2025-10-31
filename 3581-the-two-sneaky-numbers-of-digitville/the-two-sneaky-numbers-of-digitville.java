41915d0563ca628ed0dcb24affa40fbb2425b8d8
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-10-31 16:26:29 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (7.71%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]==nums[j]){
-                    ans[index++]+=nums[i];
+                    ans[index]+=nums[i];
+                    index++;
                 }
             }
         }return ans;
