538bdd66f5970a2f9a2a9e4b88b7cbfc93bafcfb
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2026-01-08 17:11:24 +0530

    Time: 1526 ms (22.79%) | Memory: 48.9 MB (36.66%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,22 +1,41 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+  
+//         int i=0,j=0;
+//         long sum=0;
+//         int max=0;
+//         while(j<nums.length){
+//             sum=sum+nums[j];
+//             if(sum<k){
+//                 j++;
+//             }else if(sum==k){
+//                 max=Math.max(max,j-i+1);
+//                 j++;
+//             }
+//             else if(sum>k){
+//                 while(sum>k&&i<=j){
+//                 sum=sum-nums[i];
+//                 i++;}
+//                 if(sum==k){
+//                     max=Math.max(max,j-i+1);
+                  
+//                 }
+//                 j++;
+//             }
+//         } return max;
+//    
          int count = 0;
         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
-            int currSum = 0;
-
-            // subarray from i to each j -> arr[i....j]
-            for (int j = i; j < n; j++) {
-                currSum += nums[j];
-
-                // increment count if the currSum equals k
-                if (currSum == k) {
-                    count++;
-                }
+       for(int i=0;i<n;i++){
+        int currsum=0;
+        for(int j=i;j<n;j++){
+            currsum+=nums[j];
+            if(currsum==k){
+                count++;
             }
-           
-        } return count;
+        }
+       } return count;
 
     }  
 }
