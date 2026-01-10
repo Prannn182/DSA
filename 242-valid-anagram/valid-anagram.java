521f049ddcaea4b7fe6e46491ca65a607afcb3a4
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2026-01-10 19:50:11 +0530

    Time: 4 ms (79.00%) | Memory: 46.5 MB (35.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-      char ch1[]=s.toCharArray();
-      char ch2[]=t.toCharArray();
-      Arrays.sort(ch1);
-      Arrays.sort(ch2);
-      String s1=new String(ch1);
-      String s2=new String(ch2);
-      return s1.equals(s2);
+    //   
+   
+    char[] ch1=s.toCharArray();
+    char[] ch2=t.toCharArray();
+    Arrays.sort(ch1);
+    Arrays.sort(ch2);
+    return Arrays.equals(ch1,ch2);
     }
 }
